635c7a4e3ab0e7371eb6f4dd4f285b15b6cc7edc
edgarmauricio123 <22620066@tlaxiaco.tecnm.mx>
2025-05-04 11:15:37 -0600

    first commit

---
+++ alumnos/src/test/java/Equipo/DMME/alumnos/AlumnosApplicationTests.java
@@ -0,0 +1,13 @@
+package Equipo.DMME.alumnos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlumnosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
