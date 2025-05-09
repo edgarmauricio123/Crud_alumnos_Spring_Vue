e9010a18bffb472206dfe215fd82ae28d46765fd
edgarmauricio123 <22620066@tlaxiaco.tecnm.mx>
2025-05-09 07:31:49 -0600

    Creación de un formulario

--- front_alumnos/src/App.vue
+++ front_alumnos/src/App.vue
@@ -2,9 +2,18 @@
 import { ref, onMounted } from 'vue';
 import axios from 'axios';
 
-const alumnos = ref({});
+const alumnos = ref([]);
+const nuevoAlumno = ref({
+  nombre: '',
+  apellido: '',
+  carrera: '',
+  telefono: '',
+  imagenURL: ''
+});
+
+
 const cargarAlumnos = async () => {
-  const response = await axios.get('http://localhost:8080/alumnos/traer-alumnos')
+  const response = await axios.get('http://localhost:3306/alumnos/traer-alumnos')
   alumnos.value = response.data;
   console.log(alumnos.value);
 }
@@ -14,37 +23,71 @@ onMounted(cargarAlumnos);
 </script>
 
 <template>
-<div class="container">
-  <h2>Tabla de alumnos </h2>
-  <table class="table">
-  <thead>
-    <tr>
-      <th scope="col"> Id</th>
-      <th scope="col">Nombre</th>
-      <th scope="col">Apellidos</th>
-      <th scope="col">Carrera</th>
-      <th scope="col">Telefono</th>
-      <th scope="col">Imagen</th>
-      <th scope="col">Acciones</th>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="card shadow p-4 mb-4">
+          <h2 class="text-center">Formulario alumnos</h2>
+          <form @submit.prevent="agregarAlumno">
+            <div class="row">
+              <div class="col-md-6 mb-3">
+                <label for="nombre" class="form-label">Nombre</label>
+                <input type="text" class="form-control" id="nombre" v-model="nuevoAlumno.nombre" required>
+              </div>
+              <div class="col-md-6 mb-3">
+                <label for="apellido" class="form-label">Apellidos</label>
+                <input type="text" class="form-control" id="apellido" v-model="nuevoAlumno.apellido" required>
+              </div>
+              <div class="col-md-6 mb-3">
+                <label for="carrera" class="form-label">Carrera</label>
+                <input type="text" class="form-control" id="carrera" v-model="nuevoAlumno.carrera" required>
+              </div>
+              <div class="col-md-6 mb-3">
+                <label for="telefono" class="form-label">Telefono</label>
+                <input type="text" class="form-control" id="telefono" v-model="nuevoAlumno.telefono" required>
+              </div>
+              <div class="col-md-6 mb-3">
+                <label for="imagenURL" class="form-label">Imagen URL</label>
+                <input type="text" class="form-control" id="imagenURL" v-model="nuevoAlumno.imagenURL" required>
+              </div>
+            </div>
+            <button type="submit" class="btn btn-primary">Agregar Alumno</button>
+          </form>
+        </div>
+      </div>
 
-    </tr>
-  </thead>
-  <tbody>
-  <tr v-for="alumno in alumnos" :key="alumno.Id">
-    <td>{{ alumno.Id }}</td>
-    <td>{{ alumno.Nombre }}</td>
-    <td>{{ alumno.Apellidos }}</td>
-    <td>{{ alumno.Carrera }}</td>
-    <td>{{ alumno.Telefono }}</td>
-    <td><img :src="alumno.Imagen" alt="Imagen de Alumno" width="50"></td>
-    <td><button class="btn btn-danger">Eliminar</button></td>
+      <div class="cold-md-2">
+        <h2>Tabla de alumnos </h2>
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col"> Id</th>
+              <th scope="col">Nombre</th>
+              <th scope="col">Apellidos</th>
+              <th scope="col">Carrera</th>
+              <th scope="col">Telefono</th>
+              <th scope="col">Imagen</th>
+              <th scope="col">Acciones</th>
 
-  </tr>
-  </tbody>
-</table>
-</div>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="alumno in alumnos" :key="alumno.Id">
+              <td>{{ alumno.Id }}</td>
+              <td>{{ alumno.Nombre }}</td>
+              <td>{{ alumno.Apellidos }}</td>
+              <td>{{ alumno.Carrera }}</td>
+              <td>{{ alumno.Telefono }}</td>
+              <td><img :src="alumno.Imagen" alt="Imagen de Alumno" width="50"></td>
+              <td><button class="btn btn-danger">Eliminar</button></td>
+
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+
+  </div>
 </template>
 z
-<style scoped>
-
-</style>
+<style scoped></style>
