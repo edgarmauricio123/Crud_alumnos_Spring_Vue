f9b5b5e3ad8df3230858495e6ff32802ab030a08
edgarmauricio123 <22620066@tlaxiaco.tecnm.mx>
2025-05-09 21:17:22 -0600

    Creación de la función de insertar formularios

--- front_alumnos/src/App.vue
+++ front_alumnos/src/App.vue
@@ -13,11 +13,21 @@ const nuevoAlumno = ref({
 
 
 const cargarAlumnos = async () => {
-  const response = await axios.get('http://localhost:3306/alumnos/traer-alumnos')
+  const response = await axios.get('http://localhost:8080/alumnos/traer-alumnos')
   alumnos.value = response.data;
   console.log(alumnos.value);
 }
-
+const agregarAlumno = async () => {
+  await axios.post('http://localhost:8080/alumnos/insertar-alumnos', nuevoAlumno.value)
+  await cargarAlumnos();
+  nuevoAlumno.value = {
+    nombre: '',
+    apellido: '',
+    carrera: '',
+    telefono: '',
+    imagenURL: ''
+  };
+}
 
 onMounted(cargarAlumnos);
 </script>
@@ -61,35 +71,35 @@ onMounted(cargarAlumnos);
           <div class="card body">
             <h5 class="card-title mb-3">Tabla de alumnos</h5>
             <table class="table table-hover align-middle">
-          <thead class="table-light">
-            <tr>
-              <th scope="col"> Id</th>
-              <th scope="col">Nombre</th>
-              <th scope="col">Apellidos</th>
-              <th scope="col">Carrera</th>
-              <th scope="col">Telefono</th>
-              <th scope="col">Imagen</th>
-              <th scope="col">Acciones</th>
+              <thead class="table-light">
+                <tr>
+                  <th scope="col"> Id</th>
+                  <th scope="col">Nombre</th>
+                  <th scope="col">Apellidos</th>
+                  <th scope="col">Carrera</th>
+                  <th scope="col">Telefono</th>
+                  <th scope="col">Imagen</th>
+                  <th scope="col">Acciones</th>
 
-            </tr>
-          </thead>  
-          <tbody>
-            <tr v-for="alumno in alumnos" :key="alumno.Id">
-              <td>{{ alumno.Id }}</td>
-              <td>{{ alumno.Nombre }}</td>
-              <td>{{ alumno.Apellidos }}</td>
-              <td>{{ alumno.Carrera }}</td>
-              <td>{{ alumno.Telefono }}</td>
-              <td><img :src="alumno.Imagen" alt="Imagen de Alumno" width="50"></td>
-              <td><button class="btn btn-danger mx-2"><i class="bi bi-trash2"></i></button></td>
-              <td><button class="btn btn-warning"><i class="bi bi-pencil-fill"></i></button></td>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="alumno in alumnos" :key="alumno.Id">
+                  <td>{{ alumno.id }}</td>
+                  <td>{{ alumno.nombre }}</td>
+                  <td>{{ alumno.apellidos }}</td>
+                  <td>{{ alumno.carrera }}</td>
+                  <td>{{ alumno.telefono }}</td>
+                  <td><img :src="alumno.Imagen" alt="Imagen de Alumno" width="50"></td>
+                  <td><button class="btn btn-danger mx-2"><i class="bi bi-trash2"></i></button></td>
+                  <td><button class="btn btn-warning"><i class="bi bi-pencil-fill"></i></button></td>
 
-            </tr>
-          </tbody>
-        </table>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
-        
+
       </div>
     </div>
 
