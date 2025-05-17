edc1c3bb5273ca5de0e0ec558e513d7f27f2f0fd
IsraelMerlyn <merlyn142a@gmail.com>
2025-05-16 18:18:08 -0600

    tasks Correccion de error de merge

--- front_alumnos/src/App.vue
+++ front_alumnos/src/App.vue
@@ -48,7 +48,7 @@ const agregarAlumno = async () => {
 const editarAlumnos = (alumno) => {
   Object.assign(nuevoAlumno.value, alumno)
   editado.value = true
-  
+
 }
 
 const eliminarAlumno = async (id) => {
@@ -77,7 +77,7 @@ const eliminarAlumno = async (id) => {
 const eliminarAlumnoPorId = async (id) => {
   try {
     await axios.delete(`http://localhost:8080/alumnos/eliminar-alumno/${id}`);
-    
+
     console.log('Alumno eliminado con id:', id);
 
     await cargarAlumnos();
@@ -105,32 +105,37 @@ onMounted(cargarAlumnos);
   <div class="container">
     <div style="color: #a4ffff;" class="row">
       <div class="col-md-12">
-        <div  class="card shadow p-4 mb-4">
+        <div class="card shadow p-4 mb-4">
           <div style="background-color: #e0daf7;" class="card">
-          <h2 class="text-center">Formulario alumnos</h2>
+            <h2 class="text-center">Formulario alumnos</h2>
           </div>
           <br>
           <form @submit.prevent="agregarAlumno">
             <div class="row">
               <div class="col-md-6 mb-3">
                 <label for="nombre" class="form-label">Nombre</label>
-                <input style="background-color: #f2ffff; " type="text" class="form-control " id="nombre" v-model="nuevoAlumno.nombre" required>
+                <input style="background-color: #f2ffff; " type="text" class="form-control " id="nombre"
+                  v-model="nuevoAlumno.nombre" required>
               </div>
               <div class="col-md-6 mb-3">
                 <label for="apellido" class="form-label">Apellidos</label>
-                <input style="background-color: #f2ffff; " type="text" class="form-control" id="apellido" v-model="nuevoAlumno.apellido" required>
+                <input style="background-color: #f2ffff; " type="text" class="form-control" id="apellido"
+                  v-model="nuevoAlumno.apellido" required>
               </div>
               <div class="col-md-6 mb-3">
                 <label for="carrera" class="form-label">Carrera</label>
-                <input style="background-color: #f2ffff; " type="text" class="form-control" id="carrera" v-model="nuevoAlumno.carrera" required>
+                <input style="background-color: #f2ffff; " type="text" class="form-control" id="carrera"
+                  v-model="nuevoAlumno.carrera" required>
               </div>
               <div class="col-md-6 mb-3">
                 <label for="telefono" class="form-label">Telefono</label>
-                <input style="background-color: #f2ffff; " type="number" class="form-control" id="telefono" v-model="nuevoAlumno.telefono" required>
+                <input style="background-color: #f2ffff; " type="number" class="form-control" id="telefono"
+                  v-model="nuevoAlumno.telefono" required>
               </div>
               <div class="col-md-6 mb-3">
                 <label for="imagenURL" class="form-label">Imagen URL</label>
-                <input style="background-color: #f2ffff; " type="text" class="form-control" id="imagenURL" v-model="nuevoAlumno.imagenURL" required>
+                <input style="background-color: #f2ffff; " type="text" class="form-control" id="imagenURL"
+                  v-model="nuevoAlumno.imagenURL" required>
               </div>
             </div>
             <button type="submit" class="btn btn-primary ">
@@ -144,39 +149,40 @@ onMounted(cargarAlumnos);
         <div class="card shadow">
           <div class="card body">
             <br>
-            
+
             <h4 class="card-title mb-3 text-center">Tabla de alumnos</h4>
             <div class="container border">
 
-            <table class="table table-hover align-middle">
-              <thead  class="table-success">
-                <tr >
-                  <th scope="col"> Id</th>
-                  <th scope="col">Nombre</th>
-                  <th scope="col">Apellidos</th>
-                  <th scope="col">Carrera</th>
-                  <th scope="col">Telefono</th>
-                  <th scope="col">Imagen</th>
-                  <th scope="col">Acciones</th>
+              <table class="table table-hover align-middle">
+                <thead class="table-success">
+                  <tr>
+                    <th scope="col"> Id</th>
+                    <th scope="col">Nombre</th>
+                    <th scope="col">Apellidos</th>
+                    <th scope="col">Carrera</th>
+                    <th scope="col">Telefono</th>
+                    <th scope="col">Imagen</th>
+                    <th scope="col">Acciones</th>
 
-                </tr>
-              </thead>
-              <tbody>
-                <tr v-for="alumno in alumnos" :key="alumno.Id">
-                  <td >{{ alumno.id }}</td>
-                  <td >{{ alumno.nombre }}</td>
-                  <td >{{ alumno.apellido }}</td>
-                  <td >{{ alumno.carrera }}</td>
-                  <td >{{ alumno.telefono }}</td>
-                  <td ><img :src="alumno.Imagen" alt="Imagen de Alumno" width="50"></td>
-                  <td ><button @click=eliminarAlumno(alumno.id) class="btn btn-danger mx-2"><i
-                        class="bi bi-trash2"></i></button>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="alumno in alumnos" :key="alumno.Id">
+                    <td>{{ alumno.id }}</td>
+                    <td>{{ alumno.nombre }}</td>
+                    <td>{{ alumno.apellido }}</td>
+                    <td>{{ alumno.carrera }}</td>
+                    <td>{{ alumno.telefono }}</td>
+                    <td><img :src="alumno.Imagen" alt="Imagen de Alumno" width="50"></td>
+                    <td><button @click=eliminarAlumno(alumno.id) class="btn btn-danger mx-2"><i
+                          class="bi bi-trash2"></i></button>
                       <button @click=editarAlumnos(alumno) class="btn btn-warning">
-                      <i class="bi bi-pencil-fill"></i></button></td>
-                 
-                </tr>
-              </tbody>
-            </table>
+                        <i class="bi bi-pencil-fill"></i></button>
+                    </td>
+
+                  </tr>
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
@@ -188,16 +194,16 @@ onMounted(cargarAlumnos);
   <br><br><br><br><br><br><br><br>
 </template>
 
-<style scoped>
-h4{
-font-family: Georgia, 'Times New Roman', Times, serif;
+<style>
+h4 {
+  font-family: Georgia, 'Times New Roman', Times, serif;
 }
-.border{
+
+.border {
   container: 10;
 }
-h2{
-font-family: Georgia, 'Times New Roman', Times, serif;
+
+h2 {
+  font-family: Georgia, 'Times New Roman', Times, serif;
 }
-
-
 </style>
